0d414bea085ce61f892774aa74e4e05b7416092d
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2025-06-29 00:28:47 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 console.time();
 
-const { app, BrowserWindow, screen } = require('electron');
+const {app, BrowserWindow, screen, protocol} = require('electron');
 const path = require('path');
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
-}
+// const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 
 const createWindow = () => {
   // Get the primary display's size
@@ -22,6 +17,7 @@ const createWindow = () => {
     frame: true,
     transparent: true,
     resizable: true,
+    
   });
 
   // and load the index.html of the app.
@@ -31,36 +27,29 @@ const createWindow = () => {
 
   mainWindow.on("ready-to-show", console.timeEnd);
   mainWindow.on("ready-to-show", mainWindow.show);
+  mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+  // installExtension(REACT_DEVELOPER_TOOLS)
+  //       .then((ext) => console.log(`Added Extension:  ${ext.name}`))
+  //       .catch((err) => console.log('An error occurred: ', err));
+})
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  };
-});
-
-// Listen for the toggle-fullscreen message
-ipcMain.on('toggle-fullscreen', (event, isFullscreen) => {
-  if (mainWindow) {
-    mainWindow.setFullScreen(isFullscreen);
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'foo', privileges: { bypassCSP: true } }
+])
